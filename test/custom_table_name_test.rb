39fa52d35f0a5171c53ecab61b40c8968b3dd2e0
Alex Coles <alex@alexcolesportfolio.com>
2010-09-01 13:30:29 +0200

    AR Parity: Add custom table name test
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

---
+++ test/custom_table_name_test.rb
@@ -0,0 +1,22 @@
+require File.expand_path('../test_helper', __FILE__)
+
+module FriendlyId
+  module Test
+    module ActiveRecordAdapter
+
+      class CustomTableNameTest < ::Test::Unit::TestCase
+
+        include FriendlyId::Test::Generic
+        include FriendlyId::Test::Slugged
+        include FriendlyId::Test::DataMapperAdapter::Slugged
+        include FriendlyId::Test::DataMapperAdapter::Core
+
+        def klass
+          Place
+        end
+
+      end
+
+    end
+  end
+end
