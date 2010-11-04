58df6c35d007d0d2396294ab854dab670b1c7b6e
Alex Coles <alex@alexbcoles.com>
2010-11-04 20:29:39 +0100

    Correct constraints when DM::Constraints is used
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- lib/friendly_id/datamapper_adapter/slugged_model.rb
+++ lib/friendly_id/datamapper_adapter/slugged_model.rb
@@ -10,11 +10,16 @@ module FriendlyId
 
       def self.included(base)
         base.class_eval do
-          has n, :slugs,
+          relationship_options = {
             :model      => ::Slug,
             :child_key  => [:sluggable_id],
             :conditions => { :sluggable_type => base },
             :order      => [:id.desc]
+          }
+          relationship_options.merge!(:constraint => :destroy) if defined?(DataMapper::Constraints)
+
+          has n, :slugs, relationship_options
+
 
           before :save do
             begin
