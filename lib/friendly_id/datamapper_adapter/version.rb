3c7703c02fc4bad52ea34ce838d163d4e832b847
Alex Coles <alex@alexbcoles.com>
2011-08-08 00:54:23 +0200

    Bump version to 3.2.0
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- lib/friendly_id/datamapper_adapter/version.rb
+++ lib/friendly_id/datamapper_adapter/version.rb
@@ -2,9 +2,9 @@ module FriendlyId
   module DataMapperAdapter
     module Version
       MAJOR  = 3
-      MINOR  = 1
+      MINOR  = 2
       TINY   = 0
-      BUILD  = 'beta1'
+      BUILD  = ''
       STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
     end
   end
